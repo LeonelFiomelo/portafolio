80b83fa886bbbc5b7f12553f8aa2421f9b854b71
Leonel Fiomelo <leonelfiomelo@hotmail.com>
2025-08-25 17:13:28 -0300

    Portafolio v1.0.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://LeonelFiomelo.github.io',
+
   integrations: [react(
     {
       include: ['**/react/*'],
